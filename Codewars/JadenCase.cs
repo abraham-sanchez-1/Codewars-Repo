fd665d82fcea9684b389beaa6f9d0c55c26c0652
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-11 20:57:05 -0500

    init

---
+++ Codewars/JadenCase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    //Jaden Case - Phrase in which all first letters are capitalized
+    // Example: "Hello, This Is Jaden Cased"
+    public static class JadenCase
+    {
+        public static string Run(string phrase)
+        {
+            // Member Variables (HAS A)
+
+            //Constructor (SPAWNER)
+
+            //Member Methods (CAN DO)
+            return phrase;
+        }
+
+    }
+}
