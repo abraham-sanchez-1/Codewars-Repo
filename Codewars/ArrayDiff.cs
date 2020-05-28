ae24c85d42e0a46ad49d97dabf6800c92fd45a3b
Abe Sanchez <sanchez95san@gmail.com>
2020-05-28 00:48:41 -0500

    New problem solved, titled ArrayDiff which removes ints from an array that are contained in another

---
+++ Codewars/ArrayDiff.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    public static class ArrayDiff
+    {
+        public static int[] MethodRun(int[] arrayOne, int[]arrayTwo)
+        {
+            // Member Variables (HAS A)
+            //Constructor (SPAWNER)
+            //Member Methods (CAN DO)
+            Console.WriteLine("This method will remove items from first array contained in the second.");
+            Console.WriteLine("First Array {0}, Second Array {1}", arrayOne, arrayTwo);
+            var newArray = arrayOne.Where(integer => !arrayTwo.Contains(integer)).ToArray();
+            Console.WriteLine("New Array: {0}", newArray);
+            Console.ReadLine();
+            return newArray;
+
+        }
+        
+
+    }
+}
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -11,7 +11,9 @@ namespace Codewars
         static void Main(string[] args)
         {
             //Jaden case class
-            JadenCase.Run();
+            //JadenCase.Run();
+
+            ArrayDiff.MethodRun(new int[] { 1, 2, 3, 4, 5 }, new int[] { 2,4});
 
 
             //Ending for all programs
