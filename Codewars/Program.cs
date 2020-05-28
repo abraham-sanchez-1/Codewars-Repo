b8bba63db290303b93dd198b8f716042c37acfe5
Abe Sanchez <sanchez95san@gmail.com>
2020-05-28 11:32:30 -0500

    Completion of regex validation problem from Codewars, only accept 4 or 6 digit code

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -13,8 +13,11 @@ namespace Codewars
             //Jaden case class
             //JadenCase.Run();
 
-            ArrayDiff.MethodRun(new int[] { 1, 2, 3, 4, 5 }, new int[] { 2,4});
+            //ArrayDiff method, removes integers from array contained in another array
+            //ArrayDiff.MethodRun(new int[] { 1, 2, 3, 4, 5 }, new int[] { 2,4});
 
+            //ATM Pin validator using Regex, will only accept a 4 or 6 digit pin, must be numbers
+            ValidatePinCode.Run();
 
             //Ending for all programs
             Console.WriteLine("End of program");
---
+++ Codewars/ValidatePinCode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Codewars
+{
+    public static class ValidatePinCode
+    {
+
+        public static void Run()
+        {
+            Console.WriteLine("This is a mock ATM pin validator, this will");
+            Console.WriteLine("only accept a 4 or 6 digit pin.");
+            Console.WriteLine("Enter Pin: ");
+            string pin = Console.ReadLine();
+            if(RegexValidate(pin))
+            {
+                Console.WriteLine("Pin Was accepted");
+            }
+            else {
+                Console.WriteLine("Pin was not accepted");
+            }
+            
+
+
+        }
+        public static bool RegexValidate(string pin)
+        {
+            return Regex.IsMatch(pin, @"^((\d{4})|(\d{6}$))\z");
+        }
+    }
+}
